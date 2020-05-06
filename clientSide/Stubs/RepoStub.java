257cc0c2103f2ef1c3ff3427cb545632facf30d8
Oscar Pimentel <oscarpimentel@ua.pt>
2020-05-06 17:02:50 +0100

    A lot of stubs doing, gets done for repo shared region stub

--- clientSide/Stubs/RepoStub.java
+++ clientSide/Stubs/RepoStub.java
@@ -6,6 +6,10 @@ import AuxTools.SimulatorParam;
 import clientSide.ClientCom;
 import clientSide.Entities.BusDriver;
 import clientSide.Entities.BusDriverState;
+import clientSide.Entities.Passenger;
+import clientSide.Entities.PassengerState;
+import clientSide.Entities.Porter;
+import clientSide.Entities.PorterState;
 
 public class RepoStub {
 	/**
@@ -48,5 +52,382 @@ public class RepoStub {
           System.exit (1);
         }
 		con.close();
-    }
+	}
+	
+	public void setPorterState(PorterState ps){
+		ClientCom con = new ClientCom (serverHostName, serverPort);
+		Message inMessage, outMessage;
+		Porter p = (Porter) Thread.currentThread();
+		//Waits for connection
+		while (!con.open ())                                    
+		{ try
+	        { p.sleep ((long) (10));
+	        }
+	        catch (InterruptedException e) {}
+	    }
+		
+		//Taking a bus message
+		outMessage = new Message (MessageType.SETPORTERSTATE, ps);
+		con.writeObject (outMessage);
+		inMessage = (Message) con.readObject ();
+		
+		if ((inMessage.getType () != MessageType.ACK))
+        { System.out.println ("Thread " + p.getName () + ": Invalid type!");
+          System.out.println (inMessage.toString ());
+          System.exit (1);
+        }
+		con.close();
+	}
+
+	public void decNumOfBagsAtPlaneHold(){
+		ClientCom con = new ClientCom (serverHostName, serverPort);
+		Message inMessage, outMessage;
+		Passenger p = (Passenger) Thread.currentThread();
+		//Waits for connection
+		while (!con.open ())                                    
+		{ try
+	        { p.sleep ((long) (10));
+	        }
+	        catch (InterruptedException e) {}
+	    }
+		
+		//Taking a bus message
+		outMessage = new Message (MessageType.DECNUMBAGSATPLANEHOLD, p);
+		con.writeObject (outMessage);
+		inMessage = (Message) con.readObject ();
+		
+		if ((inMessage.getType () != MessageType.ACK))
+        { System.out.println ("Thread " + p.getName () + ": Invalid type!");
+          System.out.println (inMessage.toString ());
+          System.exit (1);
+        }
+		con.close();
+	}
+
+	public void setFlightNumber(int flight){
+		ClientCom con = new ClientCom (serverHostName, serverPort);
+		Message inMessage, outMessage;
+		Passenger p = (Passenger) Thread.currentThread();
+		//Waits for connection
+		while (!con.open ())                                    
+		{ try
+	        { p.sleep ((long) (10));
+	        }
+	        catch (InterruptedException e) {}
+	    }
+		
+		//Passenger going home message, with the flight number
+		outMessage = new Message (MessageType.SETTINGFLIGHTNUMBER, flight);
+		con.writeObject (outMessage);
+		inMessage = (Message) con.readObject ();
+		
+		if ((inMessage.getType () != MessageType.ACK))
+        { System.out.println ("Thread " + p.getName () + ": Invalid type!");
+          System.out.println (inMessage.toString ());
+          System.exit (1);
+        }
+		con.close();
+	}
+
+	public void setNumOfBagsAtPlaneHold(int flight, int[] numOfBagsPerFlight){
+		ClientCom con = new ClientCom (serverHostName, serverPort);
+		Message inMessage, outMessage;
+		Passenger p = (Passenger) Thread.currentThread();
+		//Waits for connection
+		while (!con.open ())                                    
+		{ try
+	        { p.sleep ((long) (10));
+	        }
+	        catch (InterruptedException e) {}
+	    }
+		
+		//Passenger going home message, with the flight number
+		outMessage = new Message (MessageType.SETTINGNUMOFBAGSATPLANEHOLD, flight);
+		con.writeObject (outMessage);
+		inMessage = (Message) con.readObject ();
+		
+		if ((inMessage.getType () != MessageType.ACK))
+        { System.out.println ("Thread " + p.getName () + ": Invalid type!");
+          System.out.println (inMessage.toString ());
+          System.exit (1);
+        }
+		con.close();
+
+	}
+
+	public void setPassengersTransit(int pt){
+		ClientCom con = new ClientCom (serverHostName, serverPort);
+		Message inMessage, outMessage;
+		Passenger p = (Passenger) Thread.currentThread();
+		//Waits for connection
+		while (!con.open ())                                    
+		{ try
+	        { p.sleep ((long) (10));
+	        }
+	        catch (InterruptedException e) {}
+	    }
+		
+		//Passenger going home message, with the flight number
+		outMessage = new Message (MessageType.SETTINGPASSANGERSONTRANSIT, pt);
+		con.writeObject (outMessage);
+		inMessage = (Message) con.readObject ();
+		
+		if ((inMessage.getType () != MessageType.ACK))
+        { System.out.println ("Thread " + p.getName () + ": Invalid type!");
+          System.out.println (inMessage.toString ());
+          System.exit (1);
+        }
+		con.close();
+	}
+
+	public void setPassengerDestination(int id, String destType){
+		ClientCom con = new ClientCom (serverHostName, serverPort);
+		Message inMessage, outMessage;
+		Passenger p = (Passenger) Thread.currentThread();
+		//Waits for connection
+		while (!con.open ())                                    
+		{ try
+	        { p.sleep ((long) (10));
+	        }
+	        catch (InterruptedException e) {}
+	    }
+		
+		//Passenger going home message, with the flight number
+		outMessage = new Message (MessageType.SETTINGPASSANGERDEST, id, destType);
+		con.writeObject (outMessage);
+		inMessage = (Message) con.readObject ();
+		
+		if ((inMessage.getType () != MessageType.ACK))
+        { System.out.println ("Thread " + p.getName () + ": Invalid type!");
+          System.out.println (inMessage.toString ());
+          System.exit (1);
+        }
+		con.close();
+	}
+
+	public void setNumOfBagsAtTheBegining(int id, int nb){
+		ClientCom con = new ClientCom (serverHostName, serverPort);
+		Message inMessage, outMessage;
+		Passenger p = (Passenger) Thread.currentThread();
+		//Waits for connection
+		while (!con.open ())                                    
+		{ try
+	        { p.sleep ((long) (10));
+	        }
+	        catch (InterruptedException e) {}
+	    }
+		
+		//Passenger going home message, with the flight number
+		outMessage = new Message (MessageType.SETNUMOFBAGSATTHEBEGINNING, id, nb);
+		con.writeObject (outMessage);
+		inMessage = (Message) con.readObject ();
+		
+		if ((inMessage.getType () != MessageType.ACK))
+        { System.out.println ("Thread " + p.getName () + ": Invalid type!");
+          System.out.println (inMessage.toString ());
+          System.exit (1);
+        }
+		con.close();
+	}
+
+	public void setPassengerState(int id, PassengerState ps){
+		ClientCom con = new ClientCom (serverHostName, serverPort);
+		Message inMessage, outMessage;
+		Passenger p = (Passenger) Thread.currentThread();
+		//Waits for connection
+		while (!con.open ())                                    
+		{ try
+	        { p.sleep ((long) (10));
+	        }
+	        catch (InterruptedException e) {}
+	    }
+		
+		//Passenger going home message, with the flight number
+		outMessage = new Message (MessageType.SETTINGPASSENGERSTATE, id, ps);
+		con.writeObject (outMessage);
+		inMessage = (Message) con.readObject ();
+		
+		if ((inMessage.getType () != MessageType.ACK))
+        { System.out.println ("Thread " + p.getName () + ": Invalid type!");
+          System.out.println (inMessage.toString ());
+          System.exit (1);
+        }
+		con.close();
+	}
+
+	public void setPassengersFinalDest(int fd){
+		ClientCom con = new ClientCom (serverHostName, serverPort);
+		Message inMessage, outMessage;
+		Passenger p = (Passenger) Thread.currentThread();
+		//Waits for connection
+		while (!con.open ())                                    
+		{ try
+	        { p.sleep ((long) (10));
+	        }
+	        catch (InterruptedException e) {}
+	    }
+		
+		//Passenger going home message, with the flight number
+		outMessage = new Message (MessageType.SETTINGPASSANGERFINALDESTINATION, fd);
+		con.writeObject (outMessage);
+		inMessage = (Message) con.readObject ();
+		
+		if ((inMessage.getType () != MessageType.ACK))
+        { System.out.println ("Thread " + p.getName () + ": Invalid type!");
+          System.out.println (inMessage.toString ());
+          System.exit (1);
+        }
+		con.close();
+	}
+
+	public void setPassengersOnTheQueue(int passengersInQueue, int id){
+		ClientCom con = new ClientCom (serverHostName, serverPort);
+		Message inMessage, outMessage;
+		Passenger p = (Passenger) Thread.currentThread();
+		//Waits for connection
+		while (!con.open ())                                    
+		{ try
+	        { p.sleep ((long) (10));
+	        }
+	        catch (InterruptedException e) {}
+	    }
+		
+		//Passenger going home message, with the flight number
+		outMessage = new Message (MessageType.SETTINGPASSANGERSONTHEQUEUE, passengersInQueue, id);
+		con.writeObject (outMessage);
+		inMessage = (Message) con.readObject ();
+		
+		if ((inMessage.getType () != MessageType.ACK))
+        { System.out.println ("Thread " + p.getName () + ": Invalid type!");
+          System.out.println (inMessage.toString ());
+          System.exit (1);
+        }
+		con.close();
+	}
+
+	public void setPassangersOnTheBus(int passengersInBus, int id){
+		ClientCom con = new ClientCom (serverHostName, serverPort);
+		Message inMessage, outMessage;
+		Passenger p = (Passenger) Thread.currentThread();
+		//Waits for connection
+		while (!con.open ())                                    
+		{ try
+	        { p.sleep ((long) (10));
+	        }
+	        catch (InterruptedException e) {}
+	    }
+		
+		//Passenger going home message, with the flight number
+		outMessage = new Message (MessageType.SETTINGPASSANGERSONTHEBUS, passengersInBus, id);
+		con.writeObject (outMessage);
+		inMessage = (Message) con.readObject ();
+		
+		if ((inMessage.getType () != MessageType.ACK))
+        { System.out.println ("Thread " + p.getName () + ": Invalid type!");
+          System.out.println (inMessage.toString ());
+          System.exit (1);
+        }
+		con.close();
+	}
+
+	public void setNumOfBagsInTheConvoyBelt(int numOfBagsInConveyBelt){
+		ClientCom con = new ClientCom (serverHostName, serverPort);
+		Message inMessage, outMessage;
+		Porter p = (Porter) Thread.currentThread();
+		//Waits for connection
+		while (!con.open ())                                    
+		{ try
+	        { p.sleep ((long) (10));
+	        }
+	        catch (InterruptedException e) {}
+	    }
+		
+		//Taking a bus message
+		outMessage = new Message (MessageType.SETTINGNUMOFBAGSINTHECB, numOfBagsInConveyBelt);
+		con.writeObject (outMessage);
+		inMessage = (Message) con.readObject ();
+		
+		if ((inMessage.getType () != MessageType.ACK))
+        { System.out.println ("Thread " + p.getName () + ": Invalid type!");
+          System.out.println (inMessage.toString ());
+          System.exit (1);
+        }
+		con.close();
+	}
+
+	public void setNumOfBagsCollected(int id){
+		ClientCom con = new ClientCom (serverHostName, serverPort);
+		Message inMessage, outMessage;
+		Passenger p = (Passenger) Thread.currentThread();
+		//Waits for connection
+		while (!con.open ())                                    
+		{ try
+	        { p.sleep ((long) (10));
+	        }
+	        catch (InterruptedException e) {}
+	    }
+		
+		//Passenger going home message, with the flight number
+		outMessage = new Message (MessageType.SETTINGNUMOFBAGSCOLLECTED, id);
+		con.writeObject (outMessage);
+		inMessage = (Message) con.readObject ();
+		
+		if ((inMessage.getType () != MessageType.ACK))
+        { System.out.println ("Thread " + p.getName () + ": Invalid type!");
+          System.out.println (inMessage.toString ());
+          System.exit (1);
+        }
+		con.close();
+	}
+
+	public void setLostBags(int missingBagsTotal){
+		ClientCom con = new ClientCom (serverHostName, serverPort);
+		Message inMessage, outMessage;
+		Passenger p = (Passenger) Thread.currentThread();
+		//Waits for connection
+		while (!con.open ())                                    
+		{ try
+	        { p.sleep ((long) (10));
+	        }
+	        catch (InterruptedException e) {}
+	    }
+		
+		//Passenger going home message, with the flight number
+		outMessage = new Message (MessageType.SETTINGLOSTBAGS, missingBagsTotal);
+		con.writeObject (outMessage);
+		inMessage = (Message) con.readObject ();
+		
+		if ((inMessage.getType () != MessageType.ACK))
+        { System.out.println ("Thread " + p.getName () + ": Invalid type!");
+          System.out.println (inMessage.toString ());
+          System.exit (1);
+        }
+		con.close();
+	}
+
+	public void setNumOfBagsInTheTempArea(int numOfBagsAtStoreroom){
+		ClientCom con = new ClientCom (serverHostName, serverPort);
+		Message inMessage, outMessage;
+		Porter p = (Porter) Thread.currentThread();
+		//Waits for connection
+		while (!con.open ())                                    
+		{ try
+	        { p.sleep ((long) (10));
+	        }
+	        catch (InterruptedException e) {}
+	    }
+		
+		//Taking a bus message
+		outMessage = new Message (MessageType.SETTINGNUMBAGSTEMPAREA, numOfBagsAtStoreroom);
+		con.writeObject (outMessage);
+		inMessage = (Message) con.readObject ();
+		
+		if ((inMessage.getType () != MessageType.ACK))
+        { System.out.println ("Thread " + p.getName () + ": Invalid type!");
+          System.out.println (inMessage.toString ());
+          System.exit (1);
+        }
+		con.close();
+	}
+
 }
--- serverSide/sharedRegionInterfaces/RepoInterface.java
+++ serverSide/sharedRegionInterfaces/RepoInterface.java
@@ -27,7 +27,68 @@ public class RepoInterface {
 		 switch (inMessage.getType ()) {
 		 case SETBUSDRIVERSTATE : repo.setBusDriverState(inMessage.getBusDriverState());
 		 						  outMessage = new Message(MessageType.ACK);
-		 						  break;
+								   break;
+								   
+		case SETPORTERSTATE : repo.setPorterState(inMessage.getPorterState());
+		outMessage = new Message(MessageType.ACK);
+		break;
+
+		case DECNUMBAGSATPLANEHOLD : repo.decNumOfBagsAtPlaneHold();
+		outMessage = new Message(MessageType.ACK);
+		break;
+		
+		case SETTINGFLIGHTNUMBER : repo.setFlightNumber(inMessage.getFlight());
+		outMessage = new Message(MessageType.ACK);
+		break;
+
+		case SETTINGNUMOFBAGSATPLANEHOLD : repo.setNumOfBagsAtPlaneHold(inMessage.getFlight(), inMessage.getNumOfBagsInteger());
+		outMessage = new Message(MessageType.ACK);
+		break;
+
+		case SETTINGPASSANGERSONTRANSIT : repo.setPassengersTransit(inMessage.getPassengersInTransit());
+		outMessage = new Message(MessageType.ACK);
+		break;
+
+		case SETTINGPASSANGERDEST : repo.setPassengerDestination(inMessage.getPassengerID(), inMessage.getDestType());
+		outMessage = new Message(MessageType.ACK);
+		break;
+
+		case SETNUMOFBAGSATTHEBEGINNING : repo.setNumOfBagsAtTheBegining(inMessage.getPassengerID(), inMessage.getNumberOfBags());
+		outMessage = new Message(MessageType.ACK);
+		break;	
+
+		case SETTINGPASSENGERSTATE : repo.setPassengerState(inMessage.getPassengerID(), inMessage.getPassengerState());
+		outMessage = new Message(MessageType.ACK);
+		break;	
+		
+		case SETTINGPASSANGERFINALDESTINATION : repo.setPassengersFinalDest(inMessage.getPassengersFinalDest());
+		outMessage = new Message(MessageType.ACK);
+		break;
+
+		case SETTINGPASSANGERSONTHEQUEUE : repo.setPassengersOnTheQueue(inMessage.getPassengersOnTheQueue());
+		outMessage = new Message(MessageType.ACK);
+		break;
+
+		case SETTINGPASSANGERSONTHEBUS : repo.setPassangersOnTheBus(inMessage.getPassangersOnTheBus());
+		outMessage = new Message(MessageType.ACK);
+		break;
+
+		case SETTINGNUMOFBAGSINTHECB : repo.setNumOfBagsInTheConvoyBelt(inMessage.getNumOfBagsInTheConvoyBelt());
+		outMessage = new Message(MessageType.ACK);
+		break;
+
+		case SETTINGNUMOFBAGSCOLLECTED : repo.setNumOfBagsCollected(inMessage.getNumOfBagsCollected());
+		outMessage = new Message(MessageType.ACK);
+		break;
+
+		case SETTINGLOSTBAG : repo.setLostBags(inMessage.getLostBags());
+		outMessage = new Message(MessageType.ACK);
+		break;
+
+		case SETTINGNUMBAGSTEMPAREA : repo.setNumOfBagsInTheTempArea(inMessage.getNumOfBagsInTheTempArea());
+		outMessage = new Message(MessageType.ACK);
+		break;
+
 		 }
 		 return (outMessage);
 	}
